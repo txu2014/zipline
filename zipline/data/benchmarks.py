fcf4be5bdcd45ad9cab61b6e897587ec0d2c57da
TXu <txu2014@users.noreply.github.com>
2019-12-30 22:55:49 +0000

    skip benchmark loaing.

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -14,29 +14,40 @@
 # limitations under the License.
 import pandas as pd
 import requests
+from trading_calendars import get_calendar
 
 
-def get_benchmark_returns(symbol):
-    """
-    Get a Series of benchmark returns from IEX associated with `symbol`.
-    Default is `SPY`.
+def get_benchmark_returns(symbol, first_date, last_date):
+    cal = get_calendar('NYSE')
 
-    Parameters
-    ----------
-    symbol : str
-        Benchmark symbol for which we're getting the returns.
+    dates = cal.sessions_in_range(first_date, last_date)
 
-    The data is provided by IEX (https://iextrading.com/), and we can
-    get up to 5 years worth of data.
-    """
-    r = requests.get(
-        'https://api.iextrading.com/1.0/stock/{}/chart/5y'.format(symbol)
-    )
-    data = r.json()
+    data = pd.DataFrame(0.0, index=dates, columns=['close'])
+    data = data['close']
 
-    df = pd.DataFrame(data)
+    return data.sort_index().iloc[1:]
 
-    df.index = pd.DatetimeIndex(df['date'])
-    df = df['close']
-
-    return df.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+#
+# def get_benchmark_returns(symbol):
+#     """
+#     Get a Series of benchmark returns from IEX associated with `symbol`.
+#     Default is `SPY`.
+#
+#     Parameters
+#     ----------
+#     symbol : str
+#         Benchmark symbol for which we're getting the returns.
+#
+#     The data is provided by IEX (https://iextrading.com/), and we can
+#     get up to 5 years worth of data.
+#     """
+#     r = requests.get(
+#         'https://api.iextrading.com/1.0/stock/{}/chart/5y'.format(symbol)
+#     )
+#     data = r.json()
+#     df = pd.DataFrame(data)
+#
+#     df.index = pd.DatetimeIndex(df['date'])
+#     df = df['close']
+#
+#     return df.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
